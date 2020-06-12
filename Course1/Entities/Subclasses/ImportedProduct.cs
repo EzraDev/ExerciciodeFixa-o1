cac987e0be31bbbce874a8ca8dcfa6ee2d7cb363
Lucas Ferreira <lucas.workbr@gmail.com>
2020-06-12 11:35:33 -0300

    Projeto Finalizado

---
+++ Course1/Entities/Subclasses/ImportedProduct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Course1.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price,double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return Name + " $ " + TotalPrice().ToString("F2", CultureInfo.InvariantCulture) + "(Customs Fee: $ " + CustomsFee.ToString("F2", CultureInfo.InvariantCulture) + ")";
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+
+    }
+}
---
+++ Course1/Entities/Subclasses/UsedProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Course1.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime Date { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price, DateTime date) : base(name, price)
+        {
+            Date = date;
+        }
+
+        public override string PriceTag()
+        {
+            return Name + " (used) $ " + Price.ToString("F2", CultureInfo.InvariantCulture) + " (Manufacture date: " + Date.ToString("dd/MM/yyyy") + ")";
+        }
+    }
+}
